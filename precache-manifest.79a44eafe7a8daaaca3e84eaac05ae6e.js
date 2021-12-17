e50e702ac4574cea1c60554fd29a5f6a48c34c38
mkrajca29 <mkrajca29@gmail.com>
2021-12-17 12:48:10 +0100

    deploy - demo data

---
+++ precache-manifest.79a44eafe7a8daaaca3e84eaac05ae6e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9315950a6d13641c5c19",
+    "url": "/CryptoApp/css/about.9b48e77b.css"
+  },
+  {
+    "revision": "3348d11fb85cd3df9638",
+    "url": "/CryptoApp/css/app.5da8157e.css"
+  },
+  {
+    "revision": "9832c0ef4e6b2ae07b079f0ffeba0a29",
+    "url": "/CryptoApp/img/average.9832c0ef.svg"
+  },
+  {
+    "revision": "b11a9a268f5d12805ecdbd40771de51c",
+    "url": "/CryptoApp/img/bitcoin-dark.b11a9a26.svg"
+  },
+  {
+    "revision": "b012d9ad8a31adab89334a8b33005dbf",
+    "url": "/CryptoApp/img/bitcoin-diff.b012d9ad.svg"
+  },
+  {
+    "revision": "d71882cfd2e52ed3efbcf5bc77ce7a97",
+    "url": "/CryptoApp/img/bitcoin-purchase.d71882cf.svg"
+  },
+  {
+    "revision": "0d7ab9faa81dafff6552fd457a58a858",
+    "url": "/CryptoApp/img/bitcoin.0d7ab9fa.svg"
+  },
+  {
+    "revision": "ea6c7ed11ca679839a9bf490efac0d9e",
+    "url": "/CryptoApp/img/delete.ea6c7ed1.svg"
+  },
+  {
+    "revision": "d049ab42e7bb2f682d2be5ee9ad84fcc",
+    "url": "/CryptoApp/img/ethereum-dark.d049ab42.svg"
+  },
+  {
+    "revision": "1c96d038d83c7543bb30894025775567",
+    "url": "/CryptoApp/img/ethereum-diff.1c96d038.svg"
+  },
+  {
+    "revision": "91f8d1f96d459709f8c6cecde3731094",
+    "url": "/CryptoApp/img/ethereum-purchase.91f8d1f9.svg"
+  },
+  {
+    "revision": "57486746bde1e0390c78dd8a41e73637",
+    "url": "/CryptoApp/img/ethereum.57486746.svg"
+  },
+  {
+    "revision": "dc12ac3d5356681fe1923a79dd86d062",
+    "url": "/CryptoApp/img/global-dark.dc12ac3d.svg"
+  },
+  {
+    "revision": "474b5f093bdb41bbd54acd491fe73012",
+    "url": "/CryptoApp/img/global.474b5f09.svg"
+  },
+  {
+    "revision": "103c3d408a870656f3cf8d03558c47cd",
+    "url": "/CryptoApp/img/purchase.103c3d40.svg"
+  },
+  {
+    "revision": "3c4e6afd97802155c12995420b7419e4",
+    "url": "/CryptoApp/img/signal-0.svg"
+  },
+  {
+    "revision": "4fdd0866b1c0c6db2b34f2bb8e092d25",
+    "url": "/CryptoApp/img/signal-1.svg"
+  },
+  {
+    "revision": "b0d6d3f8ade0b4cb7bbc4323dc2837e6",
+    "url": "/CryptoApp/img/signal-2.svg"
+  },
+  {
+    "revision": "7c29e3435a5772bc0cfef7e80d57d68f",
+    "url": "/CryptoApp/img/signal-3.svg"
+  },
+  {
+    "revision": "c913756e4fce3eb138258c3073ef06c6",
+    "url": "/CryptoApp/img/signal-4.svg"
+  },
+  {
+    "revision": "8dce87348ad61f4a228b5c0d221c6b78",
+    "url": "/CryptoApp/img/switch.8dce8734.svg"
+  },
+  {
+    "revision": "45a1e47bd5f46d83986be8ba0a595129",
+    "url": "/CryptoApp/index.html"
+  },
+  {
+    "revision": "9315950a6d13641c5c19",
+    "url": "/CryptoApp/js/about.6e844424.js"
+  },
+  {
+    "revision": "3348d11fb85cd3df9638",
+    "url": "/CryptoApp/js/app.4bde4d12.js"
+  },
+  {
+    "revision": "36e5e90bdc05e28e4430",
+    "url": "/CryptoApp/js/chunk-vendors.4b3d4bf0.js"
+  },
+  {
+    "revision": "cf7216697d3c0c81721efade10594a36",
+    "url": "/CryptoApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CryptoApp/robots.txt"
+  }
+]);
